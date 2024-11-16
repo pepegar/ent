3c20aa658d0851510d0806abf7608b253ead4829
Pepe Garcia <pepe@pepegar.com>
2024-11-16 23:12:55 +0100

    add grpc impl

--- devenv.nix
+++ devenv.nix
@@ -1,7 +1,29 @@
-{pkgs, ...}: {
-  packages = [pkgs.git];
-  languages.rust.enable = true;
-  services.postgres.enable = true;
-  pre-commit.hooks.clippy.enable = true;
-  pre-commit.hooks.rustfmt.enable = true;
+{
+  pkgs,
+  lib,
+  ...
+}: {
+  packages = [
+    pkgs.git
+    pkgs.protobuf
+    pkgs.grpcurl
+  ];
+
+  languages = {
+    nix.enable = true;
+    rust = {
+      enable = true;
+      components = ["rustc" "cargo" "clippy" "rustfmt" "rust-analyzer"];
+    };
+  };
+
+  services = {
+    postgres.enable = true;
+  };
+
+  pre-commit.hooks = {
+    clippy.enable = true;
+    rustfmt.enable = true;
+    alejandra.enable = true;
+  };
 }
--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,22 @@
-mod db;
+use server::{ent::graph_service_server::GraphServiceServer, EntServer};
+use tonic::transport::Server;
+use tracing::info;
 
-use axum::{
-    http::StatusCode,
-    routing::{get, post},
-    Json, Router,
-};
-use serde::{Deserialize, Serialize};
+mod server;
 
 #[tokio::main]
-async fn main() {
-    // initialize tracing
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     tracing_subscriber::fmt::init();
 
-    // build our application with a route
-    let app = Router::new()
-        // `GET /` goes to `root`
-        .route("/", get(root))
-        // `POST /users` goes to `create_user`
-        .route("/users", post(create_user));
+    let addr = "[::1]:50051".parse()?;
+    let ent_server = EntServer::new();
 
-    // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
-}
+    info!("Server listening on {}", addr);
 
-// basic handler that responds with a static string
-async fn root() -> &'static str {
-    "Hello, World!"
-}
+    Server::builder()
+        .add_service(GraphServiceServer::new(ent_server))
+        .serve(addr)
+        .await?;
 
-async fn create_user(
-    // this argument tells axum to parse the request body
-    // as JSON into a `CreateUser` type
-    Json(payload): Json<CreateUser>,
-) -> (StatusCode, Json<User>) {
-    // insert your application logic here
-    let user = User {
-        id: 1337,
-        username: payload.username,
-    };
-
-    // this will be converted into a JSON response
-    // with a status code of `201 Created`
-    (StatusCode::CREATED, Json(user))
-}
-
-// the input to our `create_user` handler
-#[derive(Deserialize)]
-struct CreateUser {
-    username: String,
-}
-
-// the output to our `create_user` handler
-#[derive(Serialize)]
-struct User {
-    id: u64,
-    username: String,
+    Ok(())
 }
